e6f8f85a7acc17e89ce720bd7fb1b2eb18f99f68
Andrey Syzranov <Syzranov.A.Gr@sberbank.ru>
2020-07-19 12:03:39 +0300

    ES-6 added deploy config

---
+++ src/test/java/ru/easyshop/service/landing/LandingServiceTest.java
@@ -0,0 +1,11 @@
+package ru.easyshop.service.landing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.easyshop.service.StartService;
+
+public class LandingServiceTest {
+
+    @Autowired
+    StartService startService;
+
+}
